dca92f7814bc4e796209a46fad4c3bbc56c3af85
sera <sim11241124@gmail.com>
2023-01-12 11:06:16 +0900

    branch 예제

--- dags/tutorial_dag_sequence.py
+++ dags/tutorial_dag_sequence.py
@@ -9,6 +9,8 @@ from airflow.operators.python import BranchPythonOperator
 from airflow.utils.trigger_rule import TriggerRule
 
 
+#branchpythonoperator 구성 -> task형태로
+#적용될 함수 : 1,2 중 하나의 숫자를 랜덤으로 뽑아 1이면 path1, 2이면 path2로 이동
 def random_branch_path():
     # 필요 라이브러리는 여기서 import
     # https://airflow.apache.org/docs/apache-airflow/stable/best-practices.html#writing-a-dag 참고
@@ -25,7 +27,7 @@ with DAG( **dag_args ) as dag:
 
     t2 = BranchPythonOperator(
         task_id='branch',
-        python_callable=random_branch_path,
+        python_callable=random_branch_path,  #해당 인자에 파이썬 함수를 적용
     )
     
     t3 = BashOperator(
@@ -49,6 +51,7 @@ with DAG( **dag_args ) as dag:
 
     dummy_1 = DummyOperator(task_id="path1")
 
-
+    #task 관계 설정 (순서)
+    #t2가 어떤 상태냐에 따라 dummy_1혹은 t4로 가게 됨.(파이썬 함수의 결과값에 따라서)
     t1 >> t2 >> dummy_1 >> t3 >> complete
     t1 >> t2 >> t4 >> complete
